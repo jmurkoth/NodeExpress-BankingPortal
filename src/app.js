c8306b5216bb669634a5af6d4ecd9334650ade4f
Murkoth <jeevan.murkoth@intel.com>
2018-11-11 11:40:51 -0700

    Module 2

--- src/app.js
+++ src/app.js
@@ -13,9 +13,37 @@ app.set('views',path.join(__dirname,'views'));
 // set the static resource directory
 app.use(express.static(path.join(__dirname,'public')));
 
+// read the accounts
+const accountData=fs.readFileSync(path.join(__dirname,'json/accounts.json'),'utf8');
+const accounts = JSON.parse(accountData);
+
+// read the users
+const userData=fs.readFileSync(path.join(__dirname,'json/users.json'),'utf8');
+const users = JSON.parse(userData);
+
 //set the default route
 app.get('/',(req, res)=>{
-    res.render('index',{title:'Index'});
+    res.render('index',{title:'Account Summary', accounts:accounts});
+});
+
+//set the savings route
+app.get('/savings',(req, res)=>{
+    res.render('account',{account:accounts.savings});
+});
+
+//set the savings route
+app.get('/checking',(req, res)=>{
+    res.render('account',{account:accounts.checking});
+});
+
+//set the savings route
+app.get('/credit',(req, res)=>{
+    res.render('account',{account:accounts.credit});
+});
+
+//set the profile route
+app.get('/profile',(req,res)=>{
+    res.render('profile',{user:users[0]});
 });
 
 app.listen(3000,()=>{
